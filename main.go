54cd457fe73f0b1dc97eb357778c44eef786c578
Aleksey <javafirst17@gmail.com>
2018-01-03 17:27:01 +0300

    change

--- main.go
+++ main.go
@@ -2,86 +2,28 @@ package htmltojsonparser
 
 // author : https://github.com/jf17
 import (
-	"bytes"
 	"net/http"
 )
 import "io/ioutil"
 import "strings"
 
-func fromWindows1252(str string) string {
-	var arr = []byte(str)
-	var buf bytes.Buffer
-	var r rune
+func keepLines(s string, n int) string {
+	var strslice []string
 
-	for _, b := range arr {
-		switch b {
-		case 0x80:
-			r = 0x20AC
-		case 0x82:
-			r = 0x201A
-		case 0x83:
-			r = 0x0192
-		case 0x84:
-			r = 0x201E
-		case 0x85:
-			r = 0x2026
-		case 0x86:
-			r = 0x2020
-		case 0x87:
-			r = 0x2021
-		case 0x88:
-			r = 0x02C6
-		case 0x89:
-			r = 0x2030
-		case 0x8A:
-			r = 0x0160
-		case 0x8B:
-			r = 0x2039
-		case 0x8C:
-			r = 0x0152
-		case 0x8E:
-			r = 0x017D
-		case 0x91:
-			r = 0x2018
-		case 0x92:
-			r = 0x2019
-		case 0x93:
-			r = 0x201C
-		case 0x94:
-			r = 0x201D
-		case 0x95:
-			r = 0x2022
-		case 0x96:
-			r = 0x2013
-		case 0x97:
-			r = 0x2014
-		case 0x98:
-			r = 0x02DC
-		case 0x99:
-			r = 0x2122
-		case 0x9A:
-			r = 0x0161
-		case 0x9B:
-			r = 0x203A
-		case 0x9C:
-			r = 0x0153
-		case 0x9E:
-			r = 0x017E
-		case 0x9F:
-			r = 0x0178
-		default:
-			r = rune(b)
+	result := strings.Join(strings.Split(s, "\n")[:n], "\n")
+
+	str := strings.Replace(result, "\r", "", -1)
+
+	strArray := strings.Split(str, "\n")
+
+	for _, num := range strArray {
+		if strings.Contains(num, "<span class=\"hour\">") {
+			strslice = append(strslice, num)
 		}
 
-		buf.WriteRune(r)
 	}
 
-	return string(buf.Bytes())
-}
-
-func keepLines(s string, n int) string {
-	result := strings.Join(strings.Split(s, "\n")[:n], "\n")
-	return strings.Replace(result, "\r", "", -1)
+	return strings.Join(strslice)
 }
 
 func Parse(url string) string {
@@ -95,7 +37,7 @@ func Parse(url string) string {
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 
-	result = fromWindows1252(keepLines(string(body), 162))
+	result = keepLines(string(body), 162)
 
 	return result
 
