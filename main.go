214bd13a304410fb38cdaf3483eac933e37c5544
Aleksey <javafirst17@gmail.com>
2018-01-03 16:46:07 +0300

    change Parse method

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package htmltojsonparser
 
 // author : https://github.com/jf17
+import (
+	"net/http"
+)
+import "io/ioutil"
+import "strings"
 
-func Parse() string {
+
+func keepLines(s string, n int) string {
+	result := strings.Join(strings.Split(s, "\n")[:n], "\n")
+	return strings.Replace(result, "\r", "", -1)
+}
+
+func Parse(url string) string {
 
 	var result string = "html-to-json-parser"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+
+	result = keepLines(string(body), 40)
 
 	return result
 
