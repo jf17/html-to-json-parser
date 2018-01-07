cc65b202ef0a17c975d9c880ad9e175105fe6f9f
Aleksey <javafirst17@gmail.com>
2018-01-07 03:29:52 +0300

    add example

--- example/main.go
+++ example/main.go
@@ -6,4 +6,9 @@ import (
 
 func main() {
 
+	fmt.Println("Расписание автобусного маршрута 17 от остановки Троицк (мкр. \"В\") (к/ст, выс., пос.) до остановки Торговый центр (выс.)")
+	fmt.Println(" сайта Мосгортранс ")
+	fmt.Println(" парсится версия \"для печати\" ")
+	fmt.Println(htmltojsonparser.Parse("http://www.mosgortrans.org/pass3/shedule.printable.php?type=avto&way=17&date=1111100&direction=AB&waypoint=6"))
+
 }
